f7b78fc97a3471cbbe841d3584525287653096f2
Eric Olsson <eric.olsson@docusign.com>
2018-12-01 11:31:00 -0600

    Complete day 1
    This is a brute-force solution that takes minutes to run. Look for
    a better solution if time.

--- src/Day1.fs
+++ src/Day1.fs
@@ -4,3 +4,18 @@ module Day1
 
     let parseInput input =
         System.Int32.Parse(input, System.Globalization.NumberStyles.AllowLeadingSign)
+
+    let continuousInput =
+        let singleInput = readInput |> Array.map parseInput
+
+        seq { while true do yield! singleInput }
+
+    let findFrequency n =
+        continuousInput
+        |> Seq.take n
+        |> Seq.fold ( + ) 0
+
+    let evaluateFrequency (allFrequencies : System.Collections.Generic.HashSet<int>) n =
+        let freq = findFrequency n
+        let isFrequencyNew = allFrequencies.Add(freq)
+        (freq, isFrequencyNew)
--- src/Program.fs
+++ src/Program.fs
@@ -9,5 +9,14 @@ let main argv =
                              |> Array.sumBy parseInput
 
     printfn "Day  1 : The final frequency is %d" day1FinalFrequency
+
+    let allFrequencies = new System.Collections.Generic.HashSet<int>()
+
+    let firstRepeatedFrequency = continuousInput
+                                 |> Seq.mapi (fun i _ -> evaluateFrequency allFrequencies i)
+                                 |> Seq.filter (snd >> not)
+                                 |> Seq.head
+
+    printfn "Day  1 : The first repeated frequency is %d" (fst firstRepeatedFrequency)
     
     0 // return an integer exit code
