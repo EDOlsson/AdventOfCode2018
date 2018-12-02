abc502e3cbd67a5925fac1895f55a87b1dacf444
Eric Olsson <eric.olsson@docusign.com>
2018-12-02 08:40:35 -0600

    Complete day 2 part 1

--- src/Day2.fs
+++ src/Day2.fs
@@ -1,1 +1,43 @@
 module Day2
+
+    let readInput =
+        System.IO.File.ReadAllLines("day2-input", System.Text.Encoding.UTF8)
+
+    type BoxIdClassification = 
+         | HasTwoLetters
+         | HasThreeLetters
+         | HasTwoAndThreeLetters
+         | DontCare
+
+    let classifyBoxId (boxId : string) =
+        let hasRepeatedLetters (boxId' : string) count =
+            let repeated = boxId'.ToCharArray()
+                            |> Array.groupBy id
+                            |> Array.map (fun pair -> (snd pair).Length)
+                            |> Array.filter (fun len -> len = count)
+            repeated.Length > 0
+
+        match (hasRepeatedLetters boxId 2, hasRepeatedLetters boxId 3) with
+        | (true, true) -> HasTwoAndThreeLetters
+        | (true, false) -> HasTwoLetters
+        | (false, true) -> HasThreeLetters
+        | _-> DontCare
+
+    let calculateChecksum input =
+        let matchingBoxIds = input
+                             |> Array.map classifyBoxId
+                             |> Array.filter (fun c -> c <> DontCare)
+
+        let twoLetters = matchingBoxIds
+                         |> Array.filter (fun c -> c = HasTwoLetters)
+                         |> Array.length
+
+        let twoAndThreeLetters = matchingBoxIds
+                                 |> Array.filter (fun c -> c = HasTwoAndThreeLetters)
+                                 |> Array.length
+
+        let threeLetters = matchingBoxIds
+                           |> Array.filter (fun c -> c = HasThreeLetters)
+                           |> Array.length
+
+        (twoLetters + twoAndThreeLetters) * (twoAndThreeLetters + threeLetters)
--- src/Program.fs
+++ src/Program.fs
@@ -1,22 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open Day1
+open Day2
 
 [<EntryPoint>]
 let main argv =
-    let day1FinalFrequency = readInput
-                             |> Array.sumBy parseInput
+    // let day1FinalFrequency = Day1.readInput
+    //                          |> Array.sumBy Day1.parseInput
 
-    printfn "Day  1 : The final frequency is %d" day1FinalFrequency
+    // printfn "Day  1 : The final frequency is %d" day1FinalFrequency
 
-    let allFrequencies = new System.Collections.Generic.HashSet<int>()
+    // let allFrequencies = new System.Collections.Generic.HashSet<int>()
 
-    let firstRepeatedFrequency = continuousInput
-                                 |> Seq.mapi (fun i _ -> evaluateFrequency allFrequencies i)
-                                 |> Seq.filter (snd >> not)
-                                 |> Seq.head
+    // let firstRepeatedFrequency = Day1.continuousInput
+    //                              |> Seq.mapi (fun i _ -> Day1.evaluateFrequency allFrequencies i)
+    //                              |> Seq.filter (snd >> not)
+    //                              |> Seq.head
 
-    printfn "Day  1 : The first repeated frequency is %d" (fst firstRepeatedFrequency)
+    // printfn "Day  1 : The first repeated frequency is %d" (fst firstRepeatedFrequency)
+
+    let checksum = Day2.readInput
+                   |> calculateChecksum
+
+    printfn "Day  2 : The checksum is %d" checksum
     
     0 // return an integer exit code
